ab5d6e9399571634cf1978f1b1ec7b543e770655
yanis bekhtaoui <yanis.bekhtaoui@gmail.com>
2020-03-12 17:40:02 -0700

    Api Ripe connected

--- API/index.js
+++ API/index.js
@@ -3,8 +3,10 @@ app = express(),
 PORT = process.env.PORT || 8080;
 const cors = require('cors')
 const bodyParser = require('body-parser');
-const stations = require('./stations')
-const data = require('./data')
+const stations = require('./stations');
+const data = require('./data');
+var request = require("request");
+
 
 
 app.listen(PORT);
@@ -13,7 +15,17 @@ app.use(cors())
 
 app.get('/', (req, res) => {
     res.json(data);
+});
+
+
+app.get('/ping', (req, res) => {
+
+    request("https://atlas.ripe.net/api/v2/measurements/24255289/results/?format=json", function(error, response, body) {
+        res.json(body)
+    });
 })
+
+
 
 
 console.log(`todo list RESTful API server started on ${PORT}`);
